814d5c42912aea156132fca841d55294eba8547b
Jake Marwil <rjm02006@gmail.com>
2015-04-01 13:21:33 -0600

    create index and create methods with provate params

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.js   {}
+        format.json { render json: @post, status: :created, location: @post }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:text)
+    end
+
+end
